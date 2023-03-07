0e388d62f46f76dcc55ed4cc068b57969d3e0f84
Quang Trần <tranmanhquang2498it@gmail.com>
2023-03-08 06:08:59 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const sum = require("./modules/sum")
-const cachingModule = require("./modules/caching")
-const exportPartterns = require("./modules/exportPatterns")
-const nameHero = new cachingModule("lee sin")
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+
+// Register a listener
+emitter.on("order-pizza", (size, topping) => {
+  console.log(`Order received! Baking a ${size} pizza with ${topping}`);
+});
+
+// Register another listener
+emitter.on("order-pizza", (size) => {
+  if (size === "large") {
+    console.log("vao day chua");
+    console.log("Serving complimentary drink");
+  }
+});
+
+// Emit an event
+emitter.emit("order-pizza", "normal", "mushrooms");
+//! khi gọi event nó có thể chạy song song các sự kiện lắng nghe của và cùng nhận các đối số đã truyền vào
 
