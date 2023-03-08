b5545760ac8c3c5a5973fd595c872284b9eb6843
Quang Trần <tranmanhquang2498it@gmail.com>
2023-03-08 10:02:45 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const sum = require("./modules/sum")
-const cachingModule = require("./modules/caching")
-const exportPartterns = require("./modules/exportPatterns")
-const nameHero = new cachingModule("lee sin")
+class people {
+    constructor() {
+        this.age= "25",
+        this.gender = "male"
+    }
+}
 
+const callPeople = new people()
 
+class addProperti extends people {
+    constructor() {
+        super()
+        this.height="1m"
+    }
+}
+const newOj = new addProperti()
+console.log(newOj);
+//! khi extends thì đối tượng dc extends sẽ đươc kế thừa thêm chính đối tượng đó
