6bc286a7ee2f1aa8dfd017968c475d535a925266
Quang Trần <tranmanhquang2498it@gmail.com>
2023-03-08 11:02:51 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const sum = require("./modules/sum")
-const cachingModule = require("./modules/caching")
-const exportPartterns = require("./modules/exportPatterns")
-const nameHero = new cachingModule("lee sin")
+const buffer = new Buffer.from("quang test bộ đệm");
+buffer.write("Codevolution");
+
+console.log(buffer);
+console.log(buffer.toString());
+console.log(buffer.toJSON());
 
 
+//! khi tải dữ liệu nó sẽ chia theo từng đoạn dữ liệu để tránh việc khi người dùng mà không dùng hết tất cả dữ liệu đó để ko mất công
+//! khi dữ liệu chưa đủ 1 đoạn nó sẽ lưu vào bộ đệm để khi nào đủ dữ liệu 1 đoạn thì nó sẽ cho chạy 
+//! khi mà chưu đử dữ liệu của 1 đoạn thì khi hiển thì nó sẽ có cái hình loading
