132916777d26ea15805e306bb15aedfd2ff026f7
Quang Trần <tranmanhquang2498it@gmail.com>
2023-03-08 14:09:40 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const sum = require("./modules/sum")
-const cachingModule = require("./modules/caching")
-const exportPartterns = require("./modules/exportPatterns")
-const nameHero = new cachingModule("lee sin")
+const fs = require("fs");
+
+console.log("First");
+// const fileContents = fs.readFileSync("./file.txt", "utf8");
+// console.log(fileContents);
+
+console.log("Second");
+fs.readFile("./file.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
+
+console.log("Third");
 
 
+fs.writeFileSync("./file","quang test write text to file")
