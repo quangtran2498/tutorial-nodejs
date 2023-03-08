2bebeaf08056feb4bac22d6d22b1a2d21913fb31
Quang Trần <tranmanhquang2498it@gmail.com>
2023-03-08 15:13:52 +0700

    fdsfsfsd

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-
 const http = require("node:http");
+const fs = require("node:fs");
 
-const people = {
-    name:"quang",
-    age:"25"
-}
 const server = http.createServer((req, res) => {
-  res.writeHead(200,{'Content-Type': 'quang test'});
-  res.end(JSON.stringify(people));
-});
+  res.writeHead(200, { "Content-Type": "text/html" });
+  fs.createReadStream(__dirname + "/index.html").pipe(res);
 
+});
 server.listen(3000, () => {
   console.log("Server running on port 3000");
 });
